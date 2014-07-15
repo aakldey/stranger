5952e44d29f828d0787e59b7853fc3dbf4e1a82e
SemenovMikhail <fknisit@gmail.com>
2014-07-15 15:58:15 +0400

    волны

---
+++ Assets/Scripts/ParticleSystemLayer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class ParticleSystemLayer : MonoBehaviour {
+
+	public string layerName;
+
+	public int order;
+
+	// Use this for initialization
+	void Start () {
+		particleSystem.renderer.sortingLayerName = layerName;
+		particleSystem.renderer.sortingOrder = order
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/SetMeshRendererLayer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class SetMeshRendererLayer : MonoBehaviour {
+	public string LayerName;
+	public int layer = 0;
+	// Use this for initialization
+	void Start () {
+		foreach (MeshRenderer mesh in GetComponentsInChildren<MeshRenderer>()) {
+			mesh.renderer.sortingLayerName = LayerName;
+			//mesh.renderer.sortingLayerID = LayerMask.NameToLayer(LayerName);
+			mesh.renderer.sortingOrder = layer;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
